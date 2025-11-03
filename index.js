f2dc1f773fd6fdd29decd7381f3fab5bed85cdf5
min2e-crypto <hyaru3589@gmail.com>
2025-11-03 16:57:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World!");
+    return res.send("Hello World! Current Version 5.5. 2025-11-03");
 })
 
 app.get('/api', function (req, res) {
